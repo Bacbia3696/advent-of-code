0f594e530fc834432ef1533e2b6c425859dff0e8
DatNguyen <dat.n@sharering.network>
2023-03-06 14:12:21 +0700

    initial commit

---
+++ 2022/day01a/target/debug/deps/day01a-9e821b9145dd0a12.d
@@ -0,0 +1,8 @@
+/Users/nguyenthanhdat/playgroud/advent-of-code/2022/day01a/target/debug/deps/day01a-9e821b9145dd0a12.rmeta: src/main.rs Cargo.toml
+
+/Users/nguyenthanhdat/playgroud/advent-of-code/2022/day01a/target/debug/deps/day01a-9e821b9145dd0a12.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=-A__CLIPPY_HACKERY__clippy::uninlined_format_args__CLIPPY_HACKERY__
